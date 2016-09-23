fc88e85b2000fd8e4919871bd3a7d60f119a7c70
nrakochy <nick.rakochy@gmail.com>
2016-09-23 16:35:43 -0500

    Initial schema

--- src/clj/bam/db/core.clj
+++ src/clj/bam/db/core.clj
@@ -1,67 +1,25 @@
 (ns bam.db.core
   (:require [datomic.api :as d]
             [mount.core :refer [defstate]]
-            [clojure.string :as str]
+            [bam.db.schema :refer [datomic-schema]]
             [bam.config :refer [env]]))
 
 (defstate conn
           :start (-> env :database-url d/connect)
           :stop (-> conn .release))
 
-(def data-types
-    {:string "string"
-    :boolean "boolean"
-    :long "long"
-    :bigint "bigint"
-    :float  "float"
-    :double "double"
-    :bigdec "bigdec"
-    :ref "ref"
-    :instant "instant"
-    :uuid "uuid"
-    :uri "uri"
-    :bytes "byte"})
+(def db-uri-base "datomic:mem://")
 
-(def data-scheme
-  [{:ns         "user"
-   :ident       "first-name"
-   :type        "string"
-   :cardinality "one"
-   :doc         "User's first name"
-   :index       true
-   :optional    false
-  }])
+(defn dev-conn []
+  (let [uri (str db-uri-base (d/squuid))]
+    (d/delete-database uri)
+    (d/create-database uri)
+    (d/connect uri)))
 
-
-(defn datomic-schema-attribute
-  "Build a datomic schema attribute. Required keys with string values- :ns :ident :type :cardinality (one or many)"
-  [{:keys [ns ident type cardinality index doc unique fulltext is-component no-history]
-    :or {index true doc "No docs provided" unique false fulltext false is-component false no-history false}}]
-  {:db/ident (keyword ns ident)
-   :db/valueType (keyword "db.type" ((keyword (str/lower-case type)) data-types))
-   :db/cardinality (keyword "db.cardinality" cardinality)
-   :db/doc doc
-   :db/index index
-   :db/unique unique
-   :db/fulltext fulltext
-   :db/isComponent is-component
-   :db/noHistory no-history
-   :db.install/_attribute :db.part/db})
-
-(defn build-datomic-schema [coll] (map datomic-schema-attribute coll))
-
-(comment
-(defn schema-lib []
-  (s/defschema Pizza
-    {:name s/Str
-     (s/optional-key :description) s/Str
-     :size (s/enum :L :M :S)
-     :origin {:country (s/enum :FI :PO)
-              :city s/Str}}))
-)
+(def db-inst (dev-conn))
 
 (defn create-schema []
-  @(d/transact conn (build-datomic-schema data-scheme)))
+    @(d/transact db-inst datomic-schema))
 
 (defn entity [conn id]
   (d/entity (d/db conn) id))
@@ -83,3 +41,6 @@
                       :where [?e :user/id ?id]]
                     (d/db conn) id)]
     (touch conn user)))
+
+
+
--- src/clj/bam/db/schema.clj
+++ src/clj/bam/db/schema.clj
@@ -1,53 +1,223 @@
 (ns bam.db.schema
   (:require [clojure.string :refer [lower-case]]
+            [datomic.api :as d]
             [schema.core :as s]))
 
 ;; dynamic symbols-> prefix needs to match required schema.core :as value
 (def sch-prefix "s")
-(def ^:dynamic ent (symbol "Schema"))
+(def ^:dynamic sch-name (symbol "Schema"))
 
 ;;
 (def data-schema
-  [{:ns         "user"
-   :ident       "first-name"
-   :type        "string"
-   :cardinality "one"
-   :doc         "User's first name"
-   :optional    true ;; Required for Schema not Datomic
-  }])
+  ;;:optional flag is for Schema not Datomic
+  [
+   ;; USER
+   {:ent         "user"
+    :ident       "username"
+    :type        "string"
+    :cardinality "one"
+    :unique      "value"
+    :doc         "Username"
+    :optional    false}
+   {:ent         "user"
+    :ident       "fullName"
+    :type        "string"
+    :cardinality "one"
+    :doc         "Name by which user wants to be known"
+    :optional    false}
+   {:ent         "user"
+    :ident       "email"
+    :type        "string"
+    :unique      "value"
+    :cardinality "many"
+    :doc         "Email accounts of users"
+    :optional    false}
+   {:ent         "user"
+    :ident       "bio"
+    :type        "string"
+    :cardinality "one"
+    :doc         "User bio"
+    :optional    true}
+   {:ent         "user"
+    :ident       "company"
+    :type        "string"
+    :cardinality "one"
+    :doc         "User Company"
+    :optional    true}
+   {:ent         "user"
+    :ident       "roles"
+    :type        "ref"
+    :cardinality "many"
+    :doc         "Role in project"
+    :optional    true}
+   {:ent         "user"
+    :ident       "authentications"
+    :type        "ref"
+    :cardinality "many"
+    :doc         "Authentication services"
+    :optional    true}
+   ;; ORGANIZATIONS
+   {:ent         "org"
+    :ident       "name"
+    :type        "string"
+    :cardinality "one"
+    :doc         "Organization name"
+    :optional    false}
+   {:ent         "org"
+    :ident       "category"
+    :type        "ref"
+    :cardinality "one"
+    :doc         "Organization category"
+    :optional    false}
+   {:ent         "org"
+    :ident       "website"
+    :type        "uri"
+    :cardinality "one"
+    :doc         "Organization website"
+    :optional    false}
+   {:ent         "org"
+    :ident       "ein"
+    :type        "string"
+    :cardinality "one"
+    :doc         "Organization IRS employment id number"
+    :optional    false}
+   ;; PROJECT
+   {:ent         "project"
+    :ident       "title"
+    :type        "string"
+    :cardinality "one"
+    :doc         "Project Name"
+    :optional    false}
+   {:ent         "project"
+    :ident       "description"
+    :type        "string"
+    :cardinality "one"
+    :doc         "Details of project"
+    :optional    false}
+   {:ent         "project"
+    :ident       "type"
+    :type        "string"
+    :cardinality "one"
+    :doc         "Type of project - on-going, feature development, start-up, etc."
+    :optional    false}
+   {:ent         "project"
+    :ident       "workScopes"
+    :type        "ref"
+    :cardinality "many"
+    :doc         "Details of project"
+    :optional    false}
+   ;; WORK SCOPE
+   {:ent         "workScope"
+    :ident       "skills"
+    :type        "ref"
+    :cardinality "many"
+    :doc         "Cf. of skills needed for the project - consultative, technological, data science-ing etc."
+    :optional    true}
+   {:ent         "workScope"
+    :ident       "description"
+    :type        "string"
+    :cardinality "one"
+    :doc         "Description of scope of work"
+    :optional    false}
+   {:ent         "workScope"
+    :ident       "budgetedHours"
+    :type        "float"
+    :cardinality "one"
+    :doc         "Requested time commitment"
+    :optional    false}
+   ;; ENUMS (?)
+   {:ent         "enum"
+    :ident       "role"
+    :type        "string"
+    :cardinality "one"
+    :doc         "Role on the project - Developer, Owner, Requester, etc."
+    :optional    false}
+   ;; Category
+   {:ent         "category"
+    :ident       "name"
+    :type        "string"
+    :cardinality "one"
+    :doc         "Org Categorization - Community Dev, Education, etc."
+    :optional    false}
+   {:ent         "category"
+    :ident       "cause"
+    :type        "string"
+    :cardinality "one"
+    :doc         "Sub-category of organization - Education -> early childhood, scholarship, youth ed, etc. "
+    :optional    false}
+   ;; SKILLS
+   {:ent         "skill"
+    :ident       "name"
+    :type        "string"
+    :cardinality "one"
+    :doc         "Skill Name - Clojure, Postgresql, Ruby, Wordpress, etc."
+    :optional    true}
+   {:ent         "skill"
+    :ident       "type"
+    :type        "string"
+    :cardinality "one"
+    :doc         "Type of skill - technology, project management, design, etc."
+    :optional    false}
+   ;; AUTHENTICATION
+   {:ent         "authentication"
+    :ident       "type"
+    :type        "string"
+    :cardinality "one"
+    :doc         "Type of auth service"
+    :optional    false}
+   {:ent         "authentication"
+    :ident       "name"
+    :type        "string"
+    :cardinality "one"
+    :doc         "Name of auth service - Githubs, Linkedin, Facebooks, etc."
+    :optional    false}
+   {:ent         "authentication"
+    :ident       "token"
+    :type        "string"
+    :cardinality "one"
+    :doc         "Logging it in with a token"
+    :optional    false}
+   ])
 
 (def data-types
   ;; Prismatic Schema (:sch) types - Any, Bool, Num, Keyword, Symbol, Int, and Str
   ;; :dat refers to Datomic types
-    {:string    {:dat "string" :sch "Str"}
-    :boolean    {:dat "boolean" :sch "Bool"}
-    :long       {:dat "long" :sch "Num"}
-    :bigint     {:dat "bigint" :sch "Int"}
-    :float      {:dat "float" :sch "Num"}
-    :double     {:dat "double" :sch "Num" }
-    :bigdec     {:dat "bigdec" :sch "Num"}
-    :ref        {:dat "ref" :sch "Ref"}
-    :instant    {:dat "instant" :sch "Any"}
-    :uuid       {:dat  "uuid" :sch "Any"}
-    :uri        {:dat "uri" :sch "Any"}
-    :bytes      {:dat "byte" :sch "Any"}})
-
+  {:string  {:dat "string" :sch "Str"}
+   :boolean {:dat "boolean" :sch "Bool"}
+   :long    {:dat "long" :sch "Num"}
+   :bigint  {:dat "bigint" :sch "Int"}
+   :float   {:dat "float" :sch "Num"}
+   :double  {:dat "double" :sch "Num"}
+   :bigdec  {:dat "bigdec" :sch "Num"}
+   :ref     {:dat "ref" :sch "Any"}
+   :instant {:dat "instant" :sch "Any"}
+   :uuid    {:dat "uuid" :sch "Any"}
+   :uri     {:dat "uri" :sch "Any"}
+   :bytes   {:dat "byte" :sch "Any"}})
 
 ;; DATOMIC
+
+(defn uniqueness-check [result uniq-attr]
+  (if uniq-attr
+    (assoc result :db/unique (keyword "db.unique" (lower-case uniq-attr)))
+     result))
+
 (defn datomic-schema-attribute
-  "Build a datomic schema attribute. Required keys with string values- :ns :ident :type :cardinality (one or many)"
-  [{:keys [ns ident type cardinality index doc unique fulltext is-component no-history]
-    :or {index true doc "No docs provided" unique false fulltext false is-component false no-history false}}]
-    {:db/ident (keyword ns ident)
-     :db/valueType (keyword "db.type" (get-in data-types [(keyword (lower-case type)) :dat]))
-     :db/cardinality (keyword "db.cardinality" (lower-case cardinality))
-     :db/doc doc
-     :db/index index
-     :db/unique unique
-     :db/fulltext fulltext
-     :db/isComponent is-component
-     :db/noHistory no-history
-     :db.install/_attribute :db.part/db})
+  "Build a datomic schema attribute. Required keys with string values- :ent :ident :type :cardinality (one or many)"
+  [{:keys [ent ident type cardinality unique index doc fulltext is-component no-history]
+    :or   {index true doc "No docs provided" fulltext false unique nil is-component false no-history false}}]
+    (->
+       {:db/id                 (d/tempid :db.part/db)
+       :db/ident              (keyword ent ident)
+       :db/valueType          (keyword "db.type" (get-in data-types [(keyword (lower-case type)) :dat]))
+       :db/cardinality        (keyword "db.cardinality" (lower-case cardinality))
+       :db/doc                doc
+       :db/index              index
+       :db/fulltext           fulltext
+       :db/isComponent        is-component
+       :db/noHistory          no-history
+       :db.install/_attribute :db.part/db}
+     (uniqueness-check unique)))
 
 (defn build-datomic-schema [coll] (map datomic-schema-attribute coll))
 
@@ -59,18 +229,20 @@
 
 (defn build-entity-attr
   "Takes a result map and a schema map to assoc required field or not"
-  [m & [{:keys [ns ident type optional] :or [optional true]}]]
-    (let [req #(s/optional-key %)]
+  [m & [{:keys [ent ident type optional] :or [optional true]}]]
+  (let [sch-optional #(s/optional-key %)]
     (let [sch-type (symbol sch-prefix (get-in data-types [(keyword (lower-case type)) :sch]))]
       (if optional
-        (assoc m (req (keyword ident)) sch-type)
+        (assoc m (sch-optional (keyword ident)) sch-type)
         (assoc m (keyword ident) sch-type)))))
 
 (defn build-schema-lib
-   [{:keys [ns ident type]}]
-     (binding [ent (symbol ns)]
-       (s/defschema ent
-         {(keyword ident)(symbol sch-prefix (get-in data-types [(keyword (lower-case type)) :sch]))
-          (s/optional-key :description) s/Str})
-      (prn (class ent))
-       ent))
+  [{:keys [ent ident type]}]
+  (binding [sch-name (symbol sch-name)]
+    (s/defschema sch-name
+      {(keyword ident)               (symbol sch-prefix (get-in data-types [(keyword (lower-case type)) :sch]))
+       (s/optional-key :description) s/Str})
+    (prn (class ent))
+    ent))
+
+(def datomic-schema (build-datomic-schema data-schema))
